d5a343952f3f5366e94922ed8cf85c9758faab2e
Carlos <cpovedasanjose@gmail.com>
2026-01-12 12:26:45 +0100

    inicio

---
+++ src/main/java/org/example/pruebaspringboot/MainController.java
@@ -0,0 +1,17 @@
+package org.example.pruebaspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/holamundo")
+class MainController {
+
+    @GetMapping("/llamada")
+    public List<String> index() {
+        return List.of("Hello cruel world","Hola mundo cruel");
+    }
+}
